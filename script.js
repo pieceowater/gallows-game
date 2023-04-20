887bc3874c0696f31a6283302e487e24fb8061e1
pieceowater <yury.lvn@mail.ru>
2023-04-21 00:41:08 +0600

    init

---
+++ script.js
@@ -0,0 +1,59 @@
+const words = ["apple", "banana", "cherry", "grape", "kiwi", "lemon", "orange", "pear", "pineapple", "strawberry", "watermelon", "blueberry", "blackberry", "raspberry", "peach", "mango", "pomegranate", "apricot", "avocado", "coconut", "fig", "guava", "lime", "lychee", "melon", "plum", "tangerine", "nectarine", "passionfruit", "persimmon", "dragonfruit", "starfruit"];
+let word = words[Math.floor(Math.random() * words.length)];
+let wordArr = [...word];
+let guessedLetters = [];
+let numGuesses = 0;
+const maxGuesses = 6;
+
+const wordElement = document.querySelector(".word");
+const guessElement = document.querySelector("#guess");
+const guessBtn = document.querySelector("#guess-btn");
+const resultElement = document.querySelector(".result");
+const imageElement = document.querySelector(".gallows-img img");
+
+const updateDisplayWord = () => {
+    let displayWord = wordArr.map(letter => guessedLetters.includes(letter) ? letter : '_').join(' ');
+    wordElement.innerText = displayWord;
+};
+
+const handleGuess = () => {
+    let guess = guessElement.value.toLowerCase();
+    if (!guess || guessedLetters.includes(guess)) {
+        return;
+    }
+    guessedLetters.push(guess);
+    if (wordArr.includes(guess)) {
+        updateDisplayWord();
+        resultElement.classList.add('good-result')
+        resultElement.innerText = `Correct!`;
+        if (!wordElement.innerText.includes("_")) {
+            resultElement.classList.add('good-result')
+            resultElement.innerText = "You Win!";
+            guessBtn.innerText = "Refresh";
+            guessBtn.setAttribute('onclick', 'window.location.reload()');
+        }
+    } else {
+        numGuesses++;
+        if (numGuesses === maxGuesses) {
+            resultElement.classList.remove('good-result')
+            resultElement.innerText = "You Lose!";
+            wordElement.innerText = word;
+            guessBtn.innerText = "Refresh";
+            guessBtn.setAttribute('onclick', 'window.location.reload()');
+        } else {
+            resultElement.classList.remove('good-result')
+            resultElement.innerText = `Incorrect Guess.\n${maxGuesses - numGuesses} guesses remaining.`;
+            updateGallowsImage();
+        }
+    }
+    guessElement.value = "";
+};
+
+guessBtn.setAttribute("onclick", 'handleGuess()');
+
+const updateGallowsImage = () => {
+    let imgNum = numGuesses;
+    imageElement.setAttribute("src", `img/gallows/${imgNum}.png`);
+};
+
+updateDisplayWord();
